f13d0787ec893aa4a2cd97684bc518774ffaf907
Khalmatov <large000@yandex.ru>
2021-11-15 23:21:20 +0300

    Добавьте файлы проекта.

---
+++ Task1/MyExpetion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task1
+{
+    class MyExpetion : Exception
+    {
+        public MyExpetion(string message) : base(message)
+        {
+
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exception[] ex = new Exception[5]
+            {
+                new IndexOutOfRangeException("Индекс находится за пределами границ массива"),
+                new ArgumentNullException("Аргумент, передаваемый в метод — null"),
+                new DivideByZeroException("Знаменатель в операции деления или целого числа Decimal равен нулю."),
+                new FormatException("Значение не находится в соответствующем формате " +
+                                    "для преобразования из строки методом преобразования"),
+                new MyExpetion("Пользовательское исключение")
+            };
+
+            for (int i = 0; i < ex.Length; i++)
+            {
+                try
+                {
+                    throw ex[i];
+                }
+                catch (Exception e) when (e is IndexOutOfRangeException)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                catch (Exception e) when (e is ArgumentNullException)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                catch (Exception e) when (e is DivideByZeroException)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                catch (Exception e) when (e is FormatException)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                catch (Exception e) when (e is MyExpetion)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+        }
+    }
+}
